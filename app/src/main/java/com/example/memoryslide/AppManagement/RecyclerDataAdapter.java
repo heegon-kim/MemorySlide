07cce04ccdd02a361482b92ae137b64529d29838
heegon-kim <sgod5303@naver.com>
2019-12-19 03:10:09 +0900

    <최종 Commit>

--- app/src/main/java/com/example/memoryslide/AppManagement/RecyclerDataAdapter.java
+++ app/src/main/java/com/example/memoryslide/AppManagement/RecyclerDataAdapter.java
@@ -17,7 +17,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.memoryslide.BlockingAppService;
 import com.example.memoryslide.R;
 
 import java.io.BufferedWriter;
